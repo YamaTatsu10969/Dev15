cc049fd2fd0d44cea5be24884530bfc9f4dc874a
yamamoto tatsuya <52.wadachi.96@gmail.com>
2019-12-14 08:06:18 +0900

    first commit

---
+++ Day1_how_to_use_Xcode_and_Swift/Hello/Hello/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hello
+//
+//  Created by Tatsuya Yamamoto on 2019/12/13.
+//  Copyright © 2019 山本竜也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
