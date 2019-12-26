7877147b07c0dda5095f5a51e325ab203c35bd8f
yamamoto tatsuya <52.wadachi.96@gmail.com>
2019-12-26 09:19:16 +0900

    add UserDefaults

---
+++ Day2_Optional_UserDefaults/README.md
@@ -0,0 +1,20 @@
+# Day2 UserDefaults と オプショナル と TableView を学ぼう
+## 復習
+- AutoLayout
+  - ボタンの配置
+  - 画像の配置
+  - ラベルの配置
+- 遷移
+  - `present(vc, animated: true)` で遷移する
+- if文
+  - true or false で飛ぶ
+- 画像
+  - 好きな画像を Assets にいれる
+  - 表示する
+
+## やること
+
+### UserDefaults
+保存の方法を知る
+
+
---
+++ Day2_Optional_UserDefaults/UserDefaultsPractice/UserDefaultsPractice/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  UserDefaultsPractice
+//
+//  Created by Tatsuya Yamamoto on 2019/12/25.
+//  Copyright © 2019 山本竜也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func tapSaveButton(_ sender: Any) {
+        
+    }
+    
+    @IBAction func tapShowButton(_ sender: Any) {
+        
+    }
+    
+}
+
